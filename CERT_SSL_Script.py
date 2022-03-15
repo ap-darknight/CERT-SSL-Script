0ded5437ce1be4297e25bdf88d87179e8696c10e
Ashutosh Pandey <pandeyashutosh150799@gmail.com>
2022-03-15 21:07:26 +0530

    Comparison of expiry dates done! cert installation and generation remains

--- CERT_SSL_Script.py
+++ CERT_SSL_Script.py
@@ -2,17 +2,38 @@ import socket
 import ssl
 import datetime
 
-hostname = 'www.python.org'
+hostnames = ['www.python.org']
 port = 443
-context = ssl.create_default_context()
-current_date = datetime.datetime.now()
-formatted_date = current_date.strftime('%b %d %H:%M:%S %Y %Z')
- 
-with socket.create_connection((hostname, port)) as sock:
-    with context.wrap_socket(sock, server_hostname=hostname) as ssock:
-        cert_expiry = ssock.getpeercert()['notAfter']
-        cert_exp_date = datetime.datetime.strptime(cert_expiry, '%b %d %H:%M:%S %Y %Z')
-        print(cert_exp_date - current_date)
-        print(formatted_date, cert_expiry)
+expire_limit = 3;
 
+def generate_new_ssl(hostame, port):
+    print("Hello");
+
+def install_new_ssl(hostame, port):
+    print("Hello---2");
+
+def expiring_soon(hostname, port):
+    context = ssl.create_default_context()
+    current_date = datetime.datetime.now()
+    formatted_date = current_date.strftime('%b %d %H:%M:%S %Y %Z')
+    cert_exp_date = 0
+    
+    with socket.create_connection((hostname, port)) as sock:
+        with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+            cert_expiry = ssock.getpeercert()['notAfter']
+            cert_exp_date = datetime.datetime.strptime(cert_expiry, '%b %d %H:%M:%S %Y %Z')
+            print(cert_exp_date - current_date)
+            print(formatted_date, cert_expiry)
+
+    days_after = int((cert_exp_date - current_date).days);
+    print(days_after)
+    if(days_after <= expire_limit): return True
+    return False
+
+if __name__ == "__main__": 
+    for host in hostnames:
+        if(expiring_soon(host, port)):
+            generate_new_ssl(host,port)
+            install_new_ssl(host,port)
+        else: print('Saved!!')
 
