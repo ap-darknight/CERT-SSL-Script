9978b52f0b0a7d21320c6dd1408cca7f12dca7c4
Ashutosh Pandey <pandeyashutosh150799@gmail.com>
2022-03-15 17:45:22 +0530

    Date Difference calculated

--- CERT_SSL_Script.py
+++ CERT_SSL_Script.py
@@ -5,9 +5,14 @@ import datetime
 hostname = 'www.python.org'
 port = 443
 context = ssl.create_default_context()
-
+current_date = datetime.datetime.now()
+formatted_date = current_date.strftime('%b %d %H:%M:%S %Y %Z')
+ 
 with socket.create_connection((hostname, port)) as sock:
     with context.wrap_socket(sock, server_hostname=hostname) as ssock:
-        print(ssock.getpeercert()['notAfter'])
-        print(datetime.datetime.now())
-print(context)
+        cert_expiry = ssock.getpeercert()['notAfter']
+        cert_exp_date = datetime.datetime.strptime(cert_expiry, '%b %d %H:%M:%S %Y %Z')
+        print(cert_exp_date - current_date)
+        print(formatted_date, cert_expiry)
+
+
